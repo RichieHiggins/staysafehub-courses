fc5f44c81508ccb653e4521b5397800d6e46d14a
Richie Higgins <richie@staysafehub.com>
2026-01-12 20:07:25 -0500

    feat: Add Crisis Management xAPI course

---
+++ courses/crisis-management/tc-config.js
@@ -0,0 +1,20 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://7qTuNyYH3fIlpWoSE_gCVLEukt2l78Za_rise"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+  "en-US": "Crisis Management"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+  "en-US": "&lt;p&gt;The phone rings. An explosion has ripped through one of your manufacturing plants. Two people have been killed, and a dozen others are injured. The media have picked up the story, and reporters are already on site.&amp;nbsp;&lt;/p&gt;&lt;p&gt;&lt;br&gt;&lt;/p&gt;&lt;p&gt;So, what do you do? How do you respond? How will you move to minimize the damage and take control of the situation? How do you lead others through a crisis&amp;mdash;and land on your feet?&lt;/p&gt;&lt;p&gt;&lt;br&gt;&lt;/p&gt;&lt;p&gt;This course will help. You&amp;#39;ll learn to prepare for, and potentially prevent, crises &lt;em&gt;before&amp;nbsp;&lt;/em&gt;they occur. And, if disaster does strike, you&amp;#39;ll get tools to help you respond and recover.&lt;/p&gt;&lt;p&gt;&lt;br&gt;&lt;/p&gt;&lt;p&gt;When you&amp;rsquo;re ready to begin, click on the first lesson below or the &lt;em&gt;&amp;ldquo;Start Course&amp;rdquo;&lt;/em&gt; button above.&amp;nbsp;&lt;/p&gt;"
+};
