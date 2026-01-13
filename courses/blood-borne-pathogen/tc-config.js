bed3ba381e5d8babcecf5fdc4606874654ec1bb0
Richie Higgins <richie@staysafehub.com>
2026-01-12 21:10:15 -0500

    feat: Add Blood Borne Pathogen Risks and Response course

---
+++ courses/blood-borne-pathogen/tc-config.js
@@ -0,0 +1,20 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://f9Ou5YUUz3AZbSSMHcwp4OungJS5UBWE_rise"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+  "en-US": "Blood Borne Pathogen Risks and Response for Service Workers (US Version)"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+  "en-US": "Have you ever wondered what to do if you encounter blood or a sharp object while working? This course equips service workers with the essential knowledge and practical skills to recognize, prevent, and respond to blood borne pathogen risks in non-medical settings. You&#39;ll learn about the most common blood borne pathogens, how to spot and avoid risky situations, and the right steps to take if an exposure occurs. Through clear explanations and real-world examples, you&#39;ll gain the confidence to protect yourself and others, following OSHA, CDC, and Cal/OSHA guidelines every step of the way."
+};
